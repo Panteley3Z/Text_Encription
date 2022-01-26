76c5dea01f230d4b7d6ba8719861097193d06115
Panteley <panteley3z@gmail.com>
2022-01-26 16:52:14 +0300

    Initial commit

---
+++ src/Encryptor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Encryptor {
+
+    Scanner sc = new Scanner(System.in);
+    String text;
+    char[] charText;
+    int x;
+
+    public void encry() {
+        System.out.print("Enter text here: ");
+        text = sc.nextLine();
+        System.out.print("Enter number from 1 to 9\n(if your choice is 1, the string will not to be encrypted): ");
+        x = sc.nextInt();
+        charText = text.toCharArray();
+
+        for ( int i = 0; i <= text.length() - 1; i++ ) {
+            charText[i] += Math.pow(x, 3) - 1;
+        }
+        System.out.println("\nEncrypted to: ");
+        System.out.println(charText);
+    }
+
+    public void decry() {
+        for ( int i = 0; i <= text.length() - 1; i++ ) {
+            charText[i] -= Math.pow(x, 3) - 1;
+        }
+        System.out.println("\nDecrypted to: ");
+        System.out.println(charText);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Encryptor encryptor = new Encryptor();
+        encryptor.encry();
+        encryptor.decry();
+    }
+}
